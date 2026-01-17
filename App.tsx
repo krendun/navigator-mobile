a226a564c71b84b7ebac50eca4a7b5d35ebfd78a
krendun <kafkabre@protonmail.com>
2026-01-17 04:27:58 -0500

    bug fixes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AuthProvider } from "./src/auth/AuthContext";
 import { AuthGate } from "./src/auth/AuthGate";
+import "./src/lib/mapbox";
 
 export default function App() {
   return (
