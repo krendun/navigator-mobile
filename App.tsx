76b722efaace3dfa98ff08d72bfb191b65773592
krendun <kafkabre@protonmail.com>
2026-01-17 00:53:25 -0500

    app launches and shows login

--- App.tsx
+++ App.tsx
@@ -1,27 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from "react";
 import { AuthGate } from "./src/auth/AuthGate";
 
 export default function App() {
   return <AuthGate />;
 }
-
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
